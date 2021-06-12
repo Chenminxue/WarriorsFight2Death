90df669c52b27e6ca54f746c13ac72f27186a3ad
WarriorFight2Death <22242@DESKTOP-0SHGUOU>
2021-06-12 21:36:37 +0200

    WarriorFight2Death

--- WarriorFight2Death.cpp
+++ WarriorFight2Death.cpp
@@ -6,3 +6,63 @@
 #include <numeric>
 #include <math.h>
 using namespace std;
+
+class Warrior {
+private:
+	int AttackMax;
+	int BlockMax;
+
+public:
+	string name;
+	int health;
+
+	Warrior(string name, int health, int AttackMax, int BlockMax);
+
+	int Attack();
+
+	int Block();
+};
+
+class Battle {
+public:
+	static void Fight(Warrior& role1, Warrior& role2);
+
+	static void CheckResult(Warrior& role1, Warrior& role2);
+
+
+};
+
+
+
+int main() {
+
+	Warrior Thor("Thor", 100, 30, 15);
+
+
+	return 0;
+}
+
+Warrior::Warrior(string name, int health, int AttackMax, int BlockMax) {
+	this->name = name;
+	this->health = health;
+	this->AttackMax = AttackMax;
+	this->BlockMax = BlockMax;
+}
+
+int Warrior::Attack() {
+	return rand() % this->AttackMax;
+}
+
+int Warrior::Block() {
+	return rand() % this->BlockMax;
+}
+
+void Battle::Fight(Warrior& role1, Warrior& role2) {
+	while (true) {
+		if()
+	}
+}
+
+void Battle::CheckResult(Warrior& role1, Warrior& role2) {
+
+}
